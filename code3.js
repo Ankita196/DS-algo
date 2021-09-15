beb6d6306047ba9896a81185f4d32ae721bc6a5b
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-09-15 08:03:08 +0000

    sum of all elements/numbers of a given array

--- code3.js
+++ code3.js
@@ -1,4 +1,7 @@
 //the sum of all elements/numbers of a given array
+
+
+//using array reduce() method
 function AddArrayElement(){
 
 var sum = arr.reduce((a, i) => {
@@ -10,3 +13,12 @@ console.log(sum)
 var arr = [1, 2, 5, 2, 20];
 
 AddArrayElement(arr)
+
+
+//using loops
+var arr = [1, 2, 5, 10, 20];
+var sum = 0;
+for (let i in arr) {
+  sum += arr[i];
+}
+console.log(sum)
