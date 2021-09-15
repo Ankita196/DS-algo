dcfb1e7e30793d2d01ce04af0b0b3c7b8009a94f
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-09-15 08:16:17 +0000

    JavaScript startsWith and endsWith

---
+++ code5.js
@@ -0,0 +1,16 @@
+let name = "Full Stack Tutorials, Latest Interview Questions and Answers!";
+
+//startsWith
+console.log(name.startsWith("Full")); // true
+console.log(name.startsWith("full")); // false
+console.log(name.startsWith("Tutorials")); // false
+console.log(name.startsWith("Tutorials", 11)); // true
+
+//endsWith
+console.log(name.endsWith("Answers!")); // true
+console.log(name.endsWith("answers!")); // false
+
+
+
+
+//Note:- Both startsWith & endsWith are case-sensitive.
