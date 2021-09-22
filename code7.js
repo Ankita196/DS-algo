133e94d96162fea8749f3df91f382a4758a5aef4
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-09-22 16:23:06 +0000

    solved

--- code7.js
+++ code7.js
@@ -4,7 +4,7 @@ console.log([...array.filter(x=>x>0),...array.filter(x=>x<0)
 //output [1,3,2,11,6,-1,-7,-5]
 
 
-console.log([...arr.filter(x=>x>0),...arr.filter(x=>x<0)
-].join(' '))
+console.log(...arr.filter(x=>x>0),...arr.filter(x=>x<0)
+)
 //1 3 2 11 6 -1 -7 -5
 
