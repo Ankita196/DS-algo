e86ea494b7d85884f21dc48c526fd97f25e4ad67
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-10-07 14:45:08 +0530

    array destructuring

--- code11.js
+++ code11.js
@@ -1,3 +1,6 @@
+
+//  ARRAY DESTRUCTING
+
 // A junior developer structure student name, skills and score 
 //in array of arrays which may not easy to read. Destruction the following 
 //array name to name, skills array to skills, scores array to scores, 
